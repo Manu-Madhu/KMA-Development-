00c4fb7e380eab166755773a1d0bade6869bf569
Manu M <manum66466@gmail.com>
2024-07-20 21:06:43 +0530

    Managing Committee page Partial Ui Completed

---
+++ src/data/Managing.js
@@ -0,0 +1,37 @@
+export const Managing=[
+    {
+        _id: 1,
+        name: "Leo Arcand",
+        designation: "desigination",
+        image: "/managing/1.png",
+        bgColor: "#fffff",
+      },
+    {
+        _id: 2,
+        name: "Leo Arcand",
+        designation: "desigination",
+        image: "/managing/2.png",
+        bgColor: "#fffff",
+      },
+    {
+        _id: 3,
+        name: "Leo Arcand",
+        designation: "desigination",
+        image: "/managing/3.png",
+        bgColor: "#fffff",
+      },
+    {
+        _id: 4,
+        name: "Leo Arcand",
+        designation: "desigination",
+        image: "/managing/4.png",
+        bgColor: "#fffff",
+      },
+    {
+        _id: 5,
+        name: "Leo Arcand",
+        designation: "desigination",
+        image: "/managing/5.png",
+        bgColor: "#fffff",
+      },
+]
