81c9c39a84833a830021954e54945919ed15b8f2
NivedGanga <nivedganga@gmail.com>
2024-07-22 22:43:12 +0530

    hells enquiry page completed

--- src/app/halls/page.jsx
+++ src/app/halls/page.jsx
@@ -6,7 +6,7 @@ import HallsRequest from '@/components/user/halls/HallsRequest'
 
 function Page() {
     return (
-        <div className='max-w-screen-xl mx-auto'>
+        <div className='max-w-screen-xl mx-auto p-3'>
             <div className='mt-10'>
                 <h1 className='text-[2.8rem] max-md:text-4xl  font-bold leading-[3.2rem] text-center'>World Convenes at Kochi's</h1>
                 <UnderlinedHeading heading={'Iconic KMA'} text={'House'} />
@@ -15,11 +15,11 @@ function Page() {
                 <p className='w-full mt-10'>
                     Right at the centre of the buzzing city, in Panampilly Nagar, where the heart of Kochi throbs, KMA House stands proud and tall. After its complete makeover, KMA House meets with international standards of excellence. With five conference halls of varied facilities, KMA House is ready to meet all your requirements with high-tech facilities of global standards to hold physical, online and hybrid meetings for your business events and functions. The aesthetically designed interiors provide an inviting ambience with elegance and grandeur, along with state-of-the-art amenities that will enhance your brand image. The location of the property, Panampilly Nagar, is ideally located and easily accessible. The address enjoys proximity to all transportation hubs of the city. Conferences, business presentations, interviews, or seminars - KMA House offers a prestigious address to hold all kinds of corporate events.
                 </p>
-                <Image className='h-[70vh] mt-10 rounded-xl object-cover' src={hallBanner} alt='Hall Banner' />
+                <Image className='h-[70vh] max-md:h-[50vh] max-sm:h-[25vh] mt-10 rounded-xl object-cover' src={hallBanner} alt='Hall Banner' />
             </div>
             <div className='pb-10'>
                 <UnderlinedHeading heading={'Our'} text={'Halls'} />
-                <div className='mt-16 grid grid-cols-3 gap-4'>
+                <div className='mt-16 grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4'>
                     <HallsCard />
                     <HallsCard />
                     <HallsCard />
--- src/components/user/halls/HallsCard.jsx
+++ src/components/user/halls/HallsCard.jsx
@@ -1,9 +1,9 @@
 import factHall from '@/../public/assets/halls/fact_hall.png'
 import Image from 'next/image'
 
-function HallsCard() {
+function HallsCard({ isInquiryHidden = false }) {
     return (
-        <div className='rounded-2xl overflow-clip h-[19rem] relative'>
+        <div className='rounded-2xl overflow-clip h-[19rem] relative max-w-[26rem]'>
             <Image src={factHall} alt='Hall Banner' className='object-cover' />
             <div className='h-22 absolute p-4 bg-gray-800/80 bottom-0 w-full backdrop-blur-xl'>
                 <h1 className='text-xl font-semibold text-white'>
@@ -14,7 +14,7 @@ function HallsCard() {
                         <span className='text-[#FF5C67] font-medium'> 8</span> Hrs. Rs. <span className='text-[#FF5C67] font-medium'> 7000</span>+GST <span className='text-[#FF5C67] font-medium'> 4</span> Hrs. Rs. <span className='text-[#FF5C67] font-medium'> 3750</span>+GST
                         Pax 20
                     </p>
-                    <button className='bg-white text-primaryColor px-4 rounded-full font-medium'>
+                    <button className={`bg-white text-primaryColor px-4 h-fit rounded-full font-medium ${isInquiryHidden ? "hidden" : ""}`}>
                         inquire
                     </button>
                 </div>
