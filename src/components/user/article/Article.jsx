4e65ea31f8de01d6af7468101bc72df5d0f04ebf
Manu M <manum66466@gmail.com>
2024-07-25 15:06:45 +0530

    Complete the article page + article detailed view

---
+++ src/components/user/article/Article.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Article = ({
+  type = "video",
+  title,
+  platform,
+  thumbnailUrl,
+  textColor = "text-[#FF5C67]",
+  link,
+}) => {
+  return (
+    <div className="rounded-2xl overflow-hidden  flex  flex-col ">
+      <img
+        src={thumbnailUrl}
+        alt="Article Thumbnail"
+        className="object-cover "
+      />
+      <div className="p-4 bg-[#2A282F]/80 w-full min-h-[10px] backdrop-blur-xl flex justify-between items-center">
+        <div className="text-sm">
+          <p style={{ color: textColor }} className={`font-bold`}>
+            {platform}
+          </p>
+          <p className="text-white text-lg">{title}</p>
+        </div>
+
+        <div
+          className={`p-2 px-5 text-sm inset-0 size-fit  rounded-full bg-white `}
+        >
+          <Link href={link?.link || ""}>{link?.name}</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
