3d5c02a442e8527f7daa776fa1054c3a50c34246
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-08-05 21:34:54 +0530

    some changes  in reports page

--- src/components/admin/reports/ReportsContent.jsx
+++ src/components/admin/reports/ReportsContent.jsx
@@ -25,19 +25,19 @@ const ReportsContent = () => {
             className="w-full h-auto rounded-3xl object-cover"
           />
           <div className="absolute inset-x-0 bottom-0 h-1/4  bg-customGray flex flex-col justify-end p-2 rounded-b-3xl ">
-            <h3 className="text-customRed text-sm ">{item.title}</h3>
-            <p className="text-white text-lg ">{item.description}</p>
-            <button className="absolute bg-[#FFFFFF] bg-opacity-90 right-2 py-2 px-4 rounded-full text-sm flex items-center mt-2">
+            <h3 className="text-customRed text-xs pl-3 pt-1 ">{item.title}</h3>
+            <p className="text-white text-sm pl-3 pb-2">{item.description}</p>
+            <button className="absolute bg-[#FFFFFF] bg-opacity-90 right-4 py-2 px-4 rounded-full text-sm flex items-center mt-2">
             <LuDownload  className="h-5 w-5 text-black mr-2" /> 
-            download
+            Download
           </button>
           </div>
           <div className="flex flex-row justify-around mt-4">
-          <button className="absolute shadow-lg  bg-opacity-90 left-2 py-2 px-4 rounded-lg text-sm flex items-center mt-2">
-          <FiEdit   className="h-5 w-5 text-black mr-2" /> 
+          <button className="absolute shadow-lg  bg-opacity-90 left-1 py-2 px-4 rounded-lg text-sm flex items-center mt-2">
+          <FiEdit   className="h-4 w-4 text-black mr-2" /> 
             Edit
           </button>
-          <button className="absolute shadow-lg  py-2 px-4 rounded-lg text-sm flex items-center mt-2">
+          <button className="absolute shadow-lg opacity-60  py-2 px-4 rounded-lg text-sm flex items-center mt-2">
           <RiDeleteBinLine   className="h-5 w-5 text-black mr-2" /> 
             Delete
           </button>
