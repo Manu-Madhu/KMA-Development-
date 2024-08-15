0afe65257b18df976bd5317148f36fd39f79cf87
knoskimo <abhijithrb91@gmail.com>
2024-08-15 14:10:01 +0530

    stats updated

--- src/components/user/Home/StatsCard.jsx
+++ src/components/user/Home/StatsCard.jsx
@@ -3,10 +3,10 @@ import vector from '../../../../public/assets/kma csr awards/Vector.png'
 import Image from 'next/image'
 function StatsCard({ title, value }) {
     return (
-        <div className='flex flex-col items-center'>
+        <div className='h-full p-14 max-lg:p-7 flex flex-col items-center bg-[#ffd6d8] '>
             <h1 className='text-[3rem] font-bold leading-[3.4rem]'>{value}</h1>
-            <p className='text-xl font-semibold'>{title}</p>
             <Image className='-mt-1' src={vector} alt='Vector' />
+            <p className='text-xl text-primaryColor font-semibold'>{title}</p>
         </div>
     )
 }
