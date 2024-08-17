1580460947b126b6a8eb3a23ca5b9af41631c23e
knoskimo <abhijithrb91@gmail.com>
2024-08-17 15:15:53 +0530

    get all events

--- src/components/admin/events/EventsGridPart.jsx
+++ src/components/admin/events/EventsGridPart.jsx
@@ -1,11 +1,30 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import EventTypeSelect from './EventsFilter'
 import EventsCardAdmin from './EventsCardAdmin'
+import useAxiosPrivate from '@/hooks/useAxiosPrivate'
+import { eventRoute } from '@/utils/Endpoint'
 
 function EventsGridPart() {
     const [data, setData] = useState([])
 
+    const axiosPrivate = useAxiosPrivate()
+
+    const getEvents = async () => {
+      try {
+        const response = await axiosPrivate.get(eventRoute);
+        console.log({ response })
+        if (response.status === 200) {
+          setData(response?.data?.events)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    useEffect(() => {
+      getEvents()
+    }, [])
     
     return (
         <div>
@@ -19,8 +38,13 @@ function EventsGridPart() {
                     <button className='shadow-md border border-gray-300 rounded-lg p-1.5 px-3 ml-5'>delete selected</button>
                 </div>
             </div>
-            <div className='w-full grid grid-cols-3'>
-                <EventsCardAdmin Name={'jon doe'} />
+            <div className='w-full flex flex-col sm:flex-row gap-4' >
+                {
+                    data?.map((item,index)=>(
+                        <EventsCardAdmin data={item} />
+
+                    ))
+                }
             </div>
         </div>
     )
--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,3 +1,5 @@
 export const baseUrl = 'http://localhost:3000'
 
 export const adminLogin = '/api/v1/auth/admin/login'
+
+export const eventRoute = '/api/v1/event'
