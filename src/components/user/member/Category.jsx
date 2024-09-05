12ecfcd828bf52230d3afbc181342fa4bf3003af
AnandhuRs619 <ananthurs619@gmail.com>
2024-09-05 18:45:28 +0530

    MemberShip contents are added and remove Registration from nav.

--- src/components/user/member/Category.jsx
+++ src/components/user/member/Category.jsx
@@ -78,26 +78,77 @@ const Category = () => {
         />
         <AccordionItem
           title="Individual Membership"
+          content={
+            <>
+              <p className="mb-2 ">
+                An individual possessing a University Degree and having
+                managerial experience of at least 3 years in an
+                industrial/commercial/service enterprise, Government/armed
+                forces or having 3 years experience in teaching management
+                subjects in a reputed educational institution may be considered
+                for individual membership, provided that an individual who falls
+                short of the prescribed qualification may also be considered for
+                membership if he/she has at least 10 years managerial experience
+                as above and fulfills all other requirements. In exceptional
+                cases the above requirements may be waived at the discretion of
+                the Managing Committee.
+              </p>
+              <p className="mb-2 ">
+              A Professional Individual Member (PIM) of AIMA (Fellow, Member and Associate Member) whose area of activity falls within the jurisdiction of KMA will automatically become a Member of the Association.
+              </p>
+            </>
+          }
           isOpen={openIndex === 1}
           onClick={() => toggleAccordion(1)}
         />
         <AccordionItem
           title="Associate Individual Membership"
+          content={
+            <>
+              <p className="mb-2 ">
+              An individual who is working in the managerial cadre in the corporate sector, or his own business of considerable magnitude, but does not have sufficient years of experience to qualify for Ordinary Individual Membership can take up Associate Individual Membership of KMA. He will not however have any voting right.
+              </p>
+            </>
+          }
           isOpen={openIndex === 2}
           onClick={() => toggleAccordion(2)}
         />
         <AccordionItem
           title="Student Membership"
+          content={
+            <>
+              <p className="mb-2 ">
+              An individual who has attained the age of 18 years and is a student of management at the Graduate or Postgraduate level in a recognized institution is eligible to become a Student Member. He should not be employed or in own business. He will not have any voting right.
+              </p>
+            </>
+          }
           isOpen={openIndex === 3}
           onClick={() => toggleAccordion(3)}
         />
         <AccordionItem
           title="Life Membership"
+          content={
+            <>
+              <p className="mb-2 ">
+              All those who are eligible to become Ordinary Individual Members shall be eligible for enrolment as Life Members of the Association if they pay the entrance fee and 10 times the prevailing Annual Subscription as a lump sum payment.
+              </p>
+              <p className="mb-2 ">
+              Those individual who had completed 60 years of age are eligible for Senior Life Membership of the Association, if they pay the entrance fee and 5 times the prevailing Annual subscription as a lump sum payment.
+              </p>
+            </>
+          }
           isOpen={openIndex === 4}
           onClick={() => toggleAccordion(4)}
         />
         <AccordionItem
           title="Startup Membership"
+          content={
+            <>
+              <p className="mb-2 ">
+              Startup Member: A Private Limited Company/ LLP/ Partnership Firm registered with the Union Ministry of Commerce and Industry and KSUM as a start-up entity, within five years of its registration as a start-up, for a period of three years.
+              </p>
+            </>
+          }
           isOpen={openIndex === 5}
           onClick={() => toggleAccordion(5)}
         />
